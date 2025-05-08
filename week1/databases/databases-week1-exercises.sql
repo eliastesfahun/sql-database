2bd4c25f14dc71bb57d44d0f8256eee9fc6c0e12
eliastesfahun <150277247+eliastesfahun@users.noreply.github.com>
2025-05-08 20:47:11 +0200

    Update databases-week1-exercises.sql

--- week1/databases/databases-week1-exercises.sql
+++ week1/databases/databases-week1-exercises.sql
@@ -4,7 +4,7 @@ FROM country
 WHERE Population > 8000000;
 
 -- 2. What are the names of countries that have “land” in their names?
-SELECT ...
+SELECT  name FROM country WHERE  Name like '%land%';
 
 -- 3. What are the names of the cities with population in between 500,000 and 1 million?
 SELECT ...
